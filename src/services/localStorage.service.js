f7dacf2cb535b21c39d84d4ab04e2a37761fdbfb
Clément <c.spileers@outlook.fr>
2022-09-14 09:22:42 +0200

    localStorage management

--- src/services/localStorage.service.js
+++ src/services/localStorage.service.js
@@ -1,3 +1,11 @@
 export function saveToken(token) {
     localStorage.setItem("token", token);
 }
+
+export function checkToken(token) {
+    return localStorage.getItem("token");
+}
+
+export function storeUser(user) {
+    localStorage.setItem("user", JSON.stringify(user));
+}
