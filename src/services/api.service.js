8009aa630415e96746a33c9b31705c7f2ae2ed57
Clément <c.spileers@outlook.fr>
2022-09-15 00:08:25 +0200

    get collaborators list from api and store them with user in state

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,22 +1,51 @@
 import axios from "axios";
-import { saveToken } from "./localStorage.service";
+import { useEffect, useState } from "react";
+import { saveToken, storeUser } from "./localStorage.service";
 
 
 axios.defaults.baseURL = 'http://localhost:7000';
 
-export async function login (email, password, navigate) {
+export async function login(email, password, navigate) {
     const { data } = axios.post("/api/login", {
-        "email" : email,
-        "password" : password
+        "email": email,
+        "password": password
     })
+        .then((res) => {
+            console.log(res.data);
+            saveToken(res.data.token);
+            storeUser(res.data.user);
+            navigate("/collaborateurs/list");
+        })
 
-    .then((res) => {
-        console.log(res.data.token)
-        saveToken(res.data.token);
-        navigate("/collaborateurs/list");
-    })
+        .catch((err) => {
+            console.error(err);
+        })
+}
 
-    .catch((err) => {
-        console.log(err);
-    })
+
+export function getCollaborators() {
+    let token = localStorage.getItem("token");
+
+    if(!token){
+      navigate("/login");
+      return
+    }
+
+    const [collabList, setCollabList] = useState([]);
+
+    useEffect(() => {
+        async function fetchCollab() {
+            const { data } = await axios.get("/api/collaborateurs", {
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`
+                }
+            })
+
+            setCollabList(data);
+            
+            }
+            fetchCollab();
+    }, [])
+
+    return collabList
 }
