8854fbc862ac1124f9b8527ac35a9f051c92835b
Clément <c.spileers@outlook.fr>
2022-09-14 09:20:41 +0200

    created main template + routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login/Login'
 import EmployeeList from './pages/EmployeeList/EmployeeList'
 
-function App() {
-
+function App() { 
   return (
     <div className="App">
       <Router>
+        <header>
+          <h1>Intranet</h1>
+          <div className='nav-bar'>
+            <nav>
+              <ul>
+                <Link to="/"><li>Liste</li></Link>
+                <Link to="/collaborateurs/ajouter"><li className={`add-employee-btn ${isAdmin ? "show" : ""}`}>Ajouter</li></Link>
+              </ul>
+            </nav>
+            <div>
+              <img src="" alt="" />
+              <span>Deconnexion</span>
+            </div>
+          </div>
+        </header>
         <Routes>
           <Route path="/login" element={<Login/>} />
           <Route path="/collaborateurs/list" element={<EmployeeList/>} />
