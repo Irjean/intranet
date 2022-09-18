f8c0704d4713a3e237a0e1f902688a541d0c8425
Spileers Clément <c.spileers@outlook.fr>
2022-09-18 16:21:04 +0200

    Filled component with data from api

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -2,17 +2,31 @@ import React from 'react'
 import "./EmployeeCard.css"
 
 function EmployeeCard(props) {
+
+  function getBirthday () {
+    const birthdate = new Date(props.employee.birthdate);
+    const birthday = new Intl.DateTimeFormat("fr-FR", { day : "numeric", month : "long" }).format(birthdate)
+    
+    return birthday
+  }
+
+  function getAge() {
+    const birthdate = new Date(props.employee.birthdate);
+    
+    return new Date().getFullYear() - birthdate.getFullYear()
+  }
+
   return (
     <div className='employee-card'>
-        <div className={`employee-post ${props.employee.service}`}>Marketing</div>
+        <div className={`employee-post ${props.employee.service}`}>{props.employee.service}</div>
         <img className='employee-photo' src={props.employee.photo} alt="Employé" />
         <div className='employee-info'>
-            <h3>{props.employee.firstname} {props.employee.lastname} (age)</h3>
+            <h3>{props.employee.firstname} {props.employee.lastname} ({getAge()})</h3>
             <p>{props.employee.city}, {props.employee.country}</p>
-            <a href="mailto:">{props.employee.email}</a>
-            <a href="tel:+">{props.employeephone}</a>
-            <p>Anniversaire : {props.employee.birthdate}</p>
-            <div>
+            <a className='employee-email' href={`mailto:${props.employee.email}`}>{props.employee.email}</a>
+            <a className='employee-phone' href={`tel:+${props.employee.phone}`}>{props.employee.phone}</a>
+            <p>Anniversaire : {getBirthday()}</p>
+            <div className='edit-buttons'>
                 <button onClick={() => {console.log(props.employee)}}>Éditer</button>
                 <button>Supprimer</button>
             </div>
