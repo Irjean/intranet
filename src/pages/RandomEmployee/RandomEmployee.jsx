5716f83a59b1ce47beaee28cfd4127be4cd3ac30
Clément <c.spileers@outlook.fr>
2022-09-18 22:35:27 +0200

    finished random employee page

--- src/pages/RandomEmployee/RandomEmployee.jsx
+++ src/pages/RandomEmployee/RandomEmployee.jsx
@@ -1,33 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import EmployeeCard from '../../components/EmployeeCard/EmployeeCard';
-import { getRandomEmployee } from '../../services/api.service'
 import "./RandomEmployee.css"
 
 function RandomEmployee() {
-    const randomEmployee = getRandomEmployee();
-    const [randEmp, setRandEmp] = useState({})
-    
-    useEffect(() => {
-        setRandEmp(randomEmployee);
-    }, [])
+    const [randEmp, setRandEmp] = useState([]);
+  
+  //Allows to fetch the random collaborator and set it in the state
+  function getRandomCollab() {
+    fetch("http://localhost:7000/api/collaborateurs/random", {
+      headers: {
+        "Authorization" : `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => setRandEmp([data]))
+  }
 
-    function showRandomEmployee() {
-        let randomEmployee = getRandomEmployee();
-        
-        setRandEmp(randomEmployee);
-        if(randomEmployee.id == undefined){
-        return
-       }
-
-        return <EmployeeCard key={randomEmployee.id} employee={randomEmployee} />
-    }
+  useEffect(() => {
+    getRandomCollab();
+  }, [])
     
   return (
     <div id='random-employee-container'>
         <h2>Bienvenue !</h2>
         <h3>Voulez-vous commencer la journée par un "Bonjour !" à l'un de vos collègues ?</h3>
-        
-        <button onClick={() => {showRandomEmployee()}}>Dire bonjour à quelqu'un d'autre</button>
+        {randEmp.length !== 0 ? randEmp.map((i) => {return <EmployeeCard key={i.id} employee={i} />}) : "Nous n'arrivons pas à trouver vos collaborateurs."}
+        <button onClick={() => {getRandomCollab()}}>Dire bonjour à quelqu'un d'autre</button>
     </div>
   )
 }
