e242eaf6655af535f94553c3f994ffb9d11c83b4
Clément <c.spileers@outlook.fr>
2022-09-18 22:36:02 +0200

    error managing

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { addUser } from "../features/collaborators";
 import { saveToken, storeUser } from "./localStorage.service";
 
 
 axios.defaults.baseURL = 'http://localhost:7000';
 
-export async function login(email, password, navigate) {
+//Checks if the credentials are correct then authorize the connection and redirect to next page
+export async function login(email, password, dispatch) {
     const { data } = axios.post("/api/login", {
         "email": email,
         "password": password
@@ -13,16 +15,17 @@ export async function login(email, password, navigate) {
         .then((res) => {
             saveToken(res.data.token);
             storeUser(res.data.user);
-            navigate("/collaborateurs/");
+            dispatch(addUser(res.data.user));
+            window.location = "/collaborateurs"
         })
 
         .catch((err) => {
-            console.error(err);
+            document.querySelector(".err-msg").innerHTML = "Identifiants incorrects";
         })
 }
 
-
-export function getCollaborators() {
+//Returns an array with all the collaborators
+export function getCollaborators(navigate) {
     let token = localStorage.getItem("token");
 
     if(!token){
@@ -36,7 +39,7 @@ export function getCollaborators() {
         async function fetchCollab() {
             const { data } = await axios.get("/api/collaborateurs", {
                 headers: {
-                    "Authorization": `Bearer ${localStorage.getItem("token")}`
+                    "Authorization": `Bearer ${token}`
                 }
             })
 
@@ -49,9 +52,10 @@ export function getCollaborators() {
     return collabList
 }
 
-export async function getRandomEmployee () {
+// Is supposed to return a random employee in an object (as shows the console.log), but returns a promise.
+export async function getRandomEmployee (setRandEmp) {
 
-            const { data } = await axios.get("/api/collaborateurs/random", {
+            await axios.get("/api/collaborateurs/random", {
                 headers: {
                     "Authorization": `Bearer ${localStorage.getItem("token")}`
                 }
@@ -59,5 +63,8 @@ export async function getRandomEmployee () {
             .then((res) => {
                 console.log(res.data)
                 return res.data
+            })
+            .catch((err) => {
+                console.log(err)
             })
 }
