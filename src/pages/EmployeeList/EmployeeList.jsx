5580fe20d7a24f15c3d3f3d9b161b93add54cf86
Clément <c.spileers@outlook.fr>
2022-09-18 22:34:07 +0200

    fixed bug where the list would stop appearing

--- src/pages/EmployeeList/EmployeeList.jsx
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -10,46 +10,48 @@ function EmployeeList() {
   const navigate = useNavigate();
   const collaborators = useSelector((store) => store.collaboratorsReducer);
   const dispatch = useDispatch();
-  const collabList = getCollaborators();
+  const collabList = getCollaborators(navigate);
 
   const [newList, setNewList] = useState([]);
   const [categoryList, setCategoryList] = useState([]);
   const [filter, setFilter] = useState("name");
-  
+
   useEffect(() => {
     dispatch(setCollaborator(collabList));
-    setNewList(collabList);
+    filterCategory("");
   }, [collabList])
 
-  function filterCategory (category) {
+  // Create a second array that keeps only the collaborators with the selected category
+  function filterCategory(category) {
     let catList = collabList.filter(i => {
-        if(category == ""){
-            return i == i
-        }
+      if (category == "") {
+        return i == i
+      }
 
-        return i.service.toLowerCase() == category.toLowerCase();
+      return i.service.toLowerCase() == category.toLowerCase();
     });
 
-    setCategoryList(catList)
+    setCategoryList(catList);
     filterList("", catList);
-}
+  }
 
+  //Create a final array that shows all the collaborators where the name or city contains the characters in the search bar 
   function filterList(search, catList) {
-    if(search.length < 1){
-        setNewList(catList);
-        return
+    if (search.length < 1) {
+      setNewList(catList);
+      return
     }
 
     let newList = [];
 
     newList = catList.filter(i => {
-        let name = `${i.firstname} ${i.lastname}`
+      let name = `${i.firstname} ${i.lastname}`
 
-        if(filter == "name"){
-            return name.toLowerCase().includes(search.toLowerCase());
-        } else {
-            return i.city.toLowerCase().includes(search.toLowerCase());
-        }
+      if (filter == "name") {
+        return name.toLowerCase().includes(search.toLowerCase());
+      } else {
+        return i.city.toLowerCase().includes(search.toLowerCase());
+      }
     })
 
     setNewList(newList);
@@ -59,17 +61,17 @@ function EmployeeList() {
   return (
     <div id="employee-list-container">
       <h2>Liste des collaborateurs</h2>
-      <div>
+      <div className='employee-list-filters'>
         <input onKeyUp={(e) => {
-            filterList(e.target.value, categoryList);
+          filterList(e.target.value, categoryList);
         }} type="text" name="search" id="employee-search" placeholder='Rechercher...' />
         <label htmlFor="search-filter">Rechercher par :</label>
-        <select onChange={(e) => {setFilter(e.target.value); document.querySelector('#employee-search').value = ""; filterList("", categoryList)}} name="search-filter" id="search-filter">
+        <select onChange={(e) => { setFilter(e.target.value); document.querySelector('#employee-search').value = ""; filterList("", categoryList) }} name="search-filter" id="search-filter">
           <option value="name">Nom</option>
           <option value="city">Ville</option>
         </select>
         <label htmlFor="category">Catégorie :</label>
-        <select onChange={(e) => {filterCategory(e.target.value); document.querySelector('#employee-search').value = "";}} name="category" id="category">
+        <select onChange={(e) => { filterCategory(e.target.value); document.querySelector('#employee-search').value = ""; }} name="category" id="category">
           <option value="">Aucune</option>
           <option value="technique">Technique</option>
           <option value="marketing">Marketing</option>
@@ -77,7 +79,7 @@ function EmployeeList() {
         </select>
       </div>
       <div className='list-container'>
-        {newList.map(i => {return <EmployeeCard key={i.id} employee={i} />})}
+        {newList.length !== 0 ? newList.map(i => { return <EmployeeCard key={i.id} employee={i} /> }) : "Aucun collaborateur ne correspond à votre recherche !"}
       </div>
     </div>
   )
